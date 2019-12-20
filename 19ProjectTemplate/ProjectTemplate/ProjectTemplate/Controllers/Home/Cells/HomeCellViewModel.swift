604a411169aa6e632d00a8eb35e96468ed49e967
Tien Le P <tien.le@asiantech.vn>
2019-12-20 11:39:48 +0700

    Update Project Template

---
+++ 19ProjectTemplate/ProjectTemplate/ProjectTemplate/Controllers/Home/Cells/HomeCellViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  HomeCellViewModel.swift
+//  ProjectTemplate
+//
+//  Created by Le Phuong Tien on 12/20/19.
+//  Copyright © 2019 Fx Studio. All rights reserved.
+//
+
+import Foundation
---
+++ 19ProjectTemplate/ProjectTemplate/ProjectTemplate/Controllers/Home/HomeViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  HomeViewModel.swift
+//  ProjectTemplate
+//
+//  Created by Le Phuong Tien on 12/20/19.
+//  Copyright © 2019 Fx Studio. All rights reserved.
+//
+
+import Foundation
